bf92e50e15473c48a7440c9940cbbfa7234b39ed
akshatjainei <jainakshat447@gmail.com>
2023-12-26 21:14:23 +0530

    outh20 google

--- app.js
+++ app.js
@@ -1,21 +1,38 @@
 const connectDB = require('./db/connect');
 const express = require('express')
 const app = express()
+const path = require('path')
+const passport = require('passport')
 const parkingLot = require('./routes/parkingLot');
+require('./auth')
 require('dotenv').config()
 
 
 app.use(express.json());
-
+app.use(express.static (path.join(__dirname,'public')))
 app.use('/api/v1/parkingLot', parkingLot);
 
 
 const PORT = process.env.PORT || 3300
 
 app.get('/' , (req, res)=>{
-    res.send('hey')
+    res.sendFile('index.html')
 })
 
+app.get('/auth/google',
+  passport.authenticate('google', { scope:
+      [ 'email', 'profile' ] }
+));
+
+app.get( '/auth/google/callback',
+    passport.authenticate( 'google', {
+        successRedirect: '/api/v1/parkingLot',
+        failureRedirect: '/auth/google/failure'
+}));
+
+app.get('/auth/google/failure',(req, res)=>{
+    res.status(500).json({msg : 'gadbad hogyi bhai'})
+})
 
 const start = async ()=>{
     try {
