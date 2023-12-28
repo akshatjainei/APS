f752ca5d3b318990f4bdb8a32bf66ab1f2a78670
akshatjainei <jainakshat447@gmail.com>
2023-12-28 23:52:20 +0530

    rm connectionString

--- db/connect.js
+++ db/connect.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose')
 
-const connectionString = 'mongodb+srv://akshatjainei:GwXBESJeYJiJ0OK3@cluster0.jwrxgrf.mongodb.net/APS?retryWrites=true&w=majority'
-
 const connectDB = (url)=>{
   return mongoose
   .connect(url)
