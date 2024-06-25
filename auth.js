a9383bb385c00f87d3e8d4a5264d27d65bb92624
akshatjainei <jainakshat447@gmail.com>
2024-06-25 23:43:31 +0530

    JWT Auth

--- auth.js
+++ auth.js
@@ -5,7 +5,7 @@ const User = require('./models/user');
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://localhost:3300/auth/google/callback"  
+  callbackURL: "http://localhost:3300/parkingLot"  
 },
 async (token, tokenSecret, profile, done) => {
   try {
