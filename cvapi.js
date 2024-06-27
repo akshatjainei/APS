e21152aae4862e96f4647fcf366de58365a8c1e9
akshatjainei <jainakshat447@gmail.com>
2024-06-27 16:25:50 +0530

    compose for cv_model

--- cvapi.js
+++ cvapi.js
@@ -29,7 +29,7 @@ const path = require('path');
 
 async function callFastAPI() {
     try {
-        const response = await axios.get('http://127.0.0.1:8000/output');
+        const response = await axios.get('http://localhost/output');
         return response.data
     } catch (error) {
         return {msg : error}
