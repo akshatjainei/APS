890b5f3128e8368d712acb2d65d19285dfc28ee7
akshatjainei <jainakshat447@gmail.com>
2024-06-29 19:11:14 +0530

    Admin portal

---
+++ admin-server/server.js
@@ -0,0 +1,50 @@
+const express = require('express')
+const { ApolloServer } = require('@apollo/server')
+const bodyParser = require('body-parser')
+const {expressMiddleware} = require('@apollo/server/express4')
+const cors = require('cors')
+const axios = require('axios')
+
+async function startServer() {
+    const app = express()
+    const server = new ApolloServer({
+        typeDefs:`
+           type User{
+                id : ID!,
+                name : String!
+                username : String!
+                email : String!
+                phone : String!
+                website : String!
+           } 
+
+           type Ticket{
+                id : ID!
+                user : User!
+                parkingSpot : Number!
+                timestamp : String!
+           }
+            
+           type Query{
+                getAllTickets : [Ticket]
+           }
+        `,
+        resolvers : {
+            Query : {
+              getAllTicket: ({ id }) => {
+                return tickets.find(ticket => ticket.id === id);
+              },
+            }
+        },
+    })
+
+    app.use(bodyParser.json())
+    app.use(cors())
+
+    await server.start()
+
+    app.use('/graphql' , expressMiddleware(server))
+    app.listen(8000 , ()=> console.log('Started server'))
+}
+
+startServer()
